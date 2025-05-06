670ce6171413ead8397fc0aa3739fc991c7ef694
Klugy <jaencorverajavier@gmail.com>
2025-05-06 12:56:00 +0200

    Clases creadas

---
+++ src/modelo/App.java
@@ -0,0 +1,15 @@
+package modelo;
+/*
+* Hemos creado p1 que es un perro y g1 que es un gato
+* Hemos
+* */
+public class App {
+    public static void main(String[] args) {
+        Perro p1=new Perro("Perro1");
+        Gato g1=new Gato("Gato1");
+        g1.sonido();
+        p1.sonido();
+    }
+
+
+}
